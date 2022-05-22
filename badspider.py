6202ba13c92f6066fd231a23e330447ef53a8437
u4nf <4nf@protonmail.com>
2022-05-22 11:37:55 +1000

    PREPARE FOR RECURSIVE

--- badspider.py
+++ badspider.py
@@ -6,28 +6,67 @@ import argparse
 #usage
 #badspider.py -d [domain] (https:// is added automaticaly)
 
-parser = argparse.ArgumentParser(description='A tool to spider a domain and return a list for investigation')
-parser.add_argument('-d', type=str,  help='Domain to analyse')
-args=parser.parse_args()
+def validateArgs():
+	#validate arguments and set base variables
 
-if not re.match('^(?:http://|https://).*$', args.d):
-	host = 'https://' + args.d
-else:
-	host = args.d
+	parser = argparse.ArgumentParser(description='A tool to spider a domain and return a list for investigation')
+	parser.add_argument('-d', type=str,  help='Domain to analyse')
+	args=parser.parse_args()
 
-print('Assessing ' + host + '\n')
+	if not re.match('^(?:http://|https://).*$', args.d):
+		host = 'https://' + args.d
+	else:
+		host = args.d
 
-try:
-	root = requests.get(host)
-except:
-	print('Unable to parse, check above URL for validity\naccepted - domain.com, http://domain.com, https://domain.com')
-	exit(1)
-def crawl(root):
+	return host
 
-	#urllist = re.findall('href="(https://.*?)"', root.text)
 
-	#jsList = re.findall('(?:\'|src=")(.*\.js)', root.text)
+#masterlists
+uniqueUrls = []
+uniqueJs = []
+uniqueEmail = []
 
-	emailList = re.findall('[^\w]([\w\d]*@[\w\d]*\.[\w\d]*?)', root.text)
-	print(emailList)
-crawl(root)
+def crawl(currentUrl):
+	#extract data from HTML
+
+	def noDupe(inList, UniqueList, identity, currentUrl):
+		#adds to unique list if not seen before
+
+		print('Details for ' + currentUrl)
+		count = 0
+		for i in inList:
+			if i not in UniqueList:
+				UniqueList.append(i)
+				count += 1
+
+		print(identity + '\'s found - ' + str(len(inList)) + ' // Unseen - ' + str(count))
+
+
+	def parseUrl(currentUrl):
+		#take url and return HTML
+
+		try:
+			pageObject = requests.get(currentUrl)
+		except:
+			print('Unable to parse, check above URL for validity\naccepted - domain.com, http://domain.com, https://domain.com')
+			exit(1)
+
+		return pageObject
+
+
+	pageObject = parseUrl(currentUrl)
+
+	#extract and clean data
+	urlList = re.findall('href="(https://.*?)"', pageObject.text)
+	noDupe(urlList, uniqueUrls, 'URL', currentUrl)
+
+	jsList = re.findall('(?:\'|src=")(.*\.js)', pageObject.text)
+	noDupe(jsList, uniqueJs, 'JS', currentUrl)
+
+	emailList = re.findall('[^\w]([\w\d]*@[\w\d]*\.[\w\d]*?)', pageObject.text)
+	noDupe(emailList, uniqueEmail, 'Email', currentUrl)
+
+
+currentUrl = validateArgs()
+print('Assessing ' + currentUrl + '\n')
+crawl(currentUrl)
